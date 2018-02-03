695787c93228d5764c39d802404ef922d787a570
Glooory <glooorypu@gmail.com>
2018-02-03 17:54:00 +0800

    Add menus/Tabs

---
+++ menus/Tabs/tabs.js
@@ -0,0 +1,18 @@
+document.getElementById("open-default").click();
+
+function openCity(evt, cityName) {
+    var i, tabContent, tabLinks;
+
+    tabContent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    tabLinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
