2d52b2140f430d996407607b3e23780dfe2e936c
Glooory <glooorypu@gmail.com>
2018-02-05 16:59:28 +0800

    Add menus/Side Navigation

---
+++ menus/Side Navigation/side_navigation.js
@@ -0,0 +1,9 @@
+function showSideNavBar() {
+    document.getElementById("nav-side").style.width = "15%";
+    document.getElementById("container").style.marginLeft = "15%";
+}
+
+function hideSideNavBar() {
+    document.getElementById("nav-side").style.width = "0";
+    document.getElementById("container").style.marginLeft = "0";
+}
