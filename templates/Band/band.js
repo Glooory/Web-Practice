cf3affc39e470b8637d78f1fa2e7bc80ec7e4093
Glooory <glooorypu@gmail.com>
2018-02-22 20:25:14 +0800

    Add templates/Band banner part

--- templates/Band/band.js
+++ templates/Band/band.js
@@ -7,3 +7,26 @@ document.getElementById("menu_btn").onclick = function() {
         menu.style.display = "block";
     }
 };
+
+var slidesCount = 1;
+var slideTitle = document.getElementById("img-des-title");
+var slideDes = document.getElementById("img-des-content");
+
+function displaySlides() {
+    var titles = ["Los Angeles", "Chicago", "New York"];
+    var des = ["The Streets will make you feel brand new", "Big lights will inspire you", "Now hear it from New York"];
+    var slides = document.getElementsByClassName("images-slide");
+    var index = slidesCount % 3;
+    for (var i = 0; i < slides.length; i++) {
+        if (i == index) {
+            slides[i].style.display = "block";
+        } else {
+            slides[i].style.display = "none";
+        }
+    }
+    slideTitle.innerHTML = titles[index];
+    slideDes.innerHTML = des[index];
+    slidesCount++;
+}
+
+window.setInterval(displaySlides, 5000);
